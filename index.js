571818d428ea27dfefdebc7348907d85a02cf4b1
SonamJagetia <sonamvjagetia@gmail.com>
2018-04-12 16:56:24 +0530

    use put method and apply validation

--- index.js
+++ index.js
@@ -19,15 +19,9 @@ app.get('/api/courses', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
-  const schema = {
-    name: Joi.string().min(3).required()
-  };
-
-  const result = Joi.validate(req.body, schema);
-
-  if (result.error) {
-    // 400 Bad request
-    res.status(400).send(result.error.details[0].message);
+  const { error } = validateCourse(req.body); //  this is equivalent to result.error
+  if ( error) {
+    res.status(400).send(error.details[0].message);
     return;
   }
 
@@ -39,6 +33,33 @@ app.post('/api/courses', (req, res) => {
   res.send(course);
 });
 
+app.put('/api/courses/:id', (req, res) => {
+  // Look up the course
+  // If not existing, return 404
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send('The course with the given ID was not found.');
+
+  // Validate
+  // If invalid, return 400 - Bad request
+  const { error } = validateCourse(req.body); //  this is equivalent to result.error
+  if ( error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
+  // Update course
+  course.name = req.body.name;
+  // Return the updated course
+  res.send(course);
+});
+
+function validateCourse(course) {
+  const schema = {
+    name: Joi.string().min(3).required()
+  };
+
+  return Joi.validate(course, schema);
+}
 
 
 // For single course => /api/courses/1
