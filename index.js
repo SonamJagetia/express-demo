9e39ff2337f47f0e411b3626e4cdb979f04b89f8
SonamJagetia <sonamvjagetia@gmail.com>
2018-04-12 15:43:18 +0530

    use post method and apply validation using Joi Api

--- index.js
+++ index.js
@@ -1,14 +1,52 @@
+const Joi = require('joi');  //Joi return as a class so first letter should be capital
 const express = require('express');
 const app = express();
+
+app.use(express.json());
+
+const courses = [
+  { id: 1, name: 'course1'},
+  { id: 2, name: 'course2'},
+  { id: 3, name: 'course3'},
+];
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
 app.get('/api/courses', (req, res) => {
-  res.send([1, 2, 3]);
+  res.send(courses);
 });
 
+app.post('/api/courses', (req, res) => {
+  const schema = {
+    name: Joi.string().min(3).required()
+  };
+
+  const result = Joi.validate(req.body, schema);
+
+  if (result.error) {
+    // 400 Bad request
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name
+  }
+  courses.push(course);
+  res.send(course);
+});
+
+
+
+// For single course => /api/courses/1
+app.get('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send('The course with the given ID was not found.');
+  res.send(course);
+});
 //PORT
 const port = process.env.PORT || 3000;
 
